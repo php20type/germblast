08125209f38046e5f2274244d00ecaf8b43953bd
Neeraj-Shah-dev <wp86.department@gmail.com>
2025-10-08 19:43:12 +0530

    changes in migration

--- database/migrations/2025_08_07_075350_create_sources_table.php
+++ database/migrations/2025_08_07_075350_create_sources_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('sources', function (Blueprint $table) {
+        Schema::create('sources', function (Blueprint $table): void {
             $table->id();
             $table->string('name');
            $table->unsignedBigInteger('channel_id')->nullable();
